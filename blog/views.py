8c513696a1671916d24b5c39bc300190c09ec0d3
leesunuk <leesunook12@naver.com>
2022-08-05 18:36:49 +0900

    수정까지 했는데 수정이 저장이 안 됨

--- blog/views.py
+++ blog/views.py
@@ -22,28 +22,31 @@ def categories_list():
     categories = get_category_model().query.all()
     return render_template("categories_list.html", user=current_user, categories=categories)
 
-@views.route("/post-list")
-def post_list():
-    return render_template("post.html", user=current_user)
+@views.route("/post-list/<int:id>")
+def post_list(id):
+    current_category = get_category_model().query.filter_by(id=id).first()
+    posts = get_post_model().query.filter_by(category_id=id)
+    return render_template("post_list.html", user=current_user, post=posts, current_category=current_category)
 
 @views.route("/create-post", methods=['GET', 'POST'])
 @login_required
 def post_create():
     if current_user.is_staff == True:
-        form = PostForm()
-        if request.method == "POST" and form.validate_on_submit():
-            post = get_post_model()(
-                title=form.title.data,
-                content=form.content.data,
-                category_id=form.category.data,
-                author_id=current_user.id,
-            )
-            db.session.add(post)
-            db.session.commit()
-            return redirect(url_for("views.home"))
-        else:
-            categories = get_category_model().query.all()
-            return render_template("create-post.html", user=current_user, categories=categories, form=form)
+        if current_user.is_staff == True:
+            form = PostForm()
+            if request.method == "POST" and form.validate_on_submit():
+                post = get_post_model()(
+                    title=form.title.data,
+                    content=form.content.data,
+                    category_id=form.category.data,
+                    author_id=current_user.id,
+                )
+                db.session.add(post)
+                db.session.commit()
+                return redirect(url_for("views.home"))
+            else:
+                categories = get_category_model().query.all()
+                return render_template("create-post.html", user=current_user, categories=categories, form=form)
     else:
         return abort(403)
     
@@ -57,9 +60,13 @@ def edit_post(id):
     
     if current_user.is_staff == True and current_user.username == post.user.username:
         if request.method == "GET":
-            return render_template("post_edit_form.html",user=current_user, post=post, categories=categories)
+            return render_template("post_edit_form.html",user=current_user, post=post, categories=categories, form=form)
         elif request.method == "POST" and form.validate_on_submit():
-            pass
+            post.title = form.title.data
+            post.content = form.content.data
+            post.category_id = int(form.category.data)
+            db.session.commit()
+            return redirect(url_for("views.home"))
     else:
         abort(403)
         
