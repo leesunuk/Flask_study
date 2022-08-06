90272e240a40df548eec3f4da3efa78004015039
leesunuk <leesunook12@naver.com>
2022-08-06 16:31:39 +0900

    5주차

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,13 +1,19 @@
-from flask import Flask
+import email, click
+from sqlalchemy.exc import IntegrityError
+from flask import Flask, abort
 from .views import views
 from .auth import auth
 from .models import DB_NAME, db, get_user_model, get_post_model, get_category_model
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import Model, SQLAlchemy
 from os import path
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from pprint import pprint
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import PasswordField, StringField
+from wtforms.validators import InputRequired
+from flask.cli import with_appcontext
 
 def create_app():
     app=Flask(__name__)
@@ -21,9 +27,50 @@ def create_app():
     admin = Admin(app, name='blog',
                   template_mode='bootstrap3')
     
-    admin.add_view(ModelView(get_user_model(), db.session))
-    admin.add_view(ModelView(get_post_model(), db.session))
-    admin.add_view(ModelView(get_category_model(), db.session))
+    class MyUserView(ModelView):
+        def is_accessible(self):
+            if current_user.is_authenticated and current_user.is_staff == True:
+                return True
+            else:
+                return abort(403)
+            
+        class CustomPasswordField(StringField):
+            def populate_obj(self, obj, name):
+                setattr(obj, name, generate_password_hash(self.data))
+            
+        form_extra_fields = {'password': CustomPasswordField('Password', validators=[InputRequired()])}
+        form_excluded_columns = {'post', 'created_at'}
+    
+    class MyPostView(ModelView):
+        def is_accessible(self):
+            if current_user.is_authenticated and current_user.is_staff == True:
+                return True
+            else:
+                return abort(403)
+        
+        form_excluded_columns = {'created_at', 'comments'}
+        
+    class MyCategoryView(ModelView):
+        def is_accessible(self):
+            if current_user.is_authenticated and current_user.is_staff == True:
+                return True
+            else:
+                return abort(403)
+        
+        form_excluded_columns = {'category'}
+    
+    class MyCommentView(ModelView):
+        def is_accessible(self):
+            if current_user.is_authenticated and current_user.is_staff == True:
+                return True
+            else:
+                return abort(403)
+        
+    admin.add_view(MyUserView(get_user_model(), db.session))
+    admin.add_view(MyPostView(get_post_model(), db.session))
+    admin.add_view(MyCategoryView(get_category_model(), db.session))
+    # admin.add_view(MyCommentView(get_comment_model(), db.session))
+    
     
     db.init_app(app)
     
@@ -40,8 +87,30 @@ def create_app():
     def load_user_by_id(id):
         return get_user_model().query.get(int(id))
     
-    return app
 
+    @click.command(name="create_superuser")
+    @with_appcontext
+    def create_superuser():
+        username = input("Enter username : ")
+        email = input("Enter email : ")
+        password = input("Enter password : ")
+        is_staff = True
+    
+        try:
+            superuser = get_user_model()(
+                username = username,
+                email = email,
+                password =generate_password_hash(password),
+                is_staff = is_staff
+            )
+            db.session.add(superuser)
+            db.session.commit()
+        except IntegrityError:
+            print('\033[31m' + "Error : username or email already exists.")
+        print(f"User created! : {email}")
+    
+    app.cli.add_command(create_superuser)
+    return app
 
 def create_database(app):
     if not path.exists("blog/"+DB_NAME):
